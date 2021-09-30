4120cd7eb9c555d3870ce202642a8d84c998361f
Rasmus Andersson <rasmus@notion.se>
2021-09-29 17:52:56 -0700

    fixes source directory scanner to look for .co files instead of .w files

--- src/co/source.c
+++ src/co/source.c
@@ -129,8 +129,11 @@ bool PkgScanSources(Pkg* pkg) {
       case DT_REG:
       case DT_LNK:
       case DT_UNKNOWN:
-        if (e.d_namlen > 3 && e.d_name[0] != '.' && strcmp(&e.d_name[e.d_namlen-2], ".w") == 0)
+        if (e.d_namlen > 3 && e.d_name[0] != '.' &&
+            strcmp(&e.d_name[e.d_namlen-2], ".co") == 0)
+        {
           ok = PkgAddFileSource(pkg, e.d_name) && ok;
+        }
         break;
       default:
         break;
